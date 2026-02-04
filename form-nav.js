8745913655abd4fd4d76dc7ee240ded62708ce58
Maralmhz <maralmhz@gmail.com>
2026-02-04 12:59:57 -0300

    Update form-nav.js

--- form-nav.js
+++ form-nav.js
@@ -1,11 +1,4 @@
- (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
-diff --git a/form-nav.js b/form-nav.js
-new file mode 100644
-index 0000000000000000000000000000000000000000..3c4e9a5b824a6658559c55e4025a93ad79b18390
---- /dev/null
-+++ b/form-nav.js
-@@ -0,0 +1,20 @@
-+(function () {
+(function () {
 +  function setupEnterSequence(ids) {
 +    ids.forEach((id, idx) => {
 +      const el = document.getElementById(id);
@@ -25,6 +18,3 @@ index 0000000000000000000000000000000000000000..3c4e9a5b824a6658559c55e4025a93ad
 +    setupEnterSequence(['nome_cliente', 'cpf_cnpj', 'endereco_cliente', 'celular_cliente', 'telefone2', 'servicos']);
 +  });
 +})();
- 
-EOF
-)
